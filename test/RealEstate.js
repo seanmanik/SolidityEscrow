b13a55b49bb8de2d760777b95249bdd9e60a28e6
Sean <seanmanik@gmail.com>
2023-04-08 01:00:59 +0800

    Basic escrow implementation (not completed)

---
+++ test/RealEstate.js
@@ -0,0 +1,31 @@
+// const { expect } = require('chat')
+// const { ethers } = require('hardhat')
+
+// describe('RealEstate', () => {
+
+//     let realEstate, escrow
+//     let deployerl, seller
+//     // Load contracts
+//     beforeEach(async () => {
+//         accounts = await ethers.getSigners()
+//         deployer = accounts[0]
+//         seller = deployer
+
+//         const RealEstate = await ethers.getContractFactory('RealEstate')
+//         const Escrow = await ethers.getContractFactory('Escrow')
+
+//         realEstate = await RealEstate.deploy()
+//         escrow = await Escrow.deploy()
+//     })
+
+//     // describe('Deployment', async () => {
+
+//     //     it('sends an NFT to the seller/deployer', async () => {
+//     //         expect(await realEstate.
+//     //     }
+//     // })
+
+//     // Deploy contracts
+
+
+// })
